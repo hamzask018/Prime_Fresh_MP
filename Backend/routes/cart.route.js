436a283031dfd0b1ff4c4b40cdfb249ff4d3c8a3
M.Hamza Shaikh <145010624+hamzask018@users.noreply.github.com>
2024-10-26 23:56:44 +0530

    Delete Backend directory

--- Backend/routes/cart.route.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-const router = Router();
-import { createOrUpdateCart, getCart, deleteCart } from '../controllers/cart.controller.js';
-
-router.post('/', createOrUpdateCart);
-router.get('/:userId', getCart);
-router.delete('/:userId', deleteCart);
-
-export default router;
--- Backend/routes/payment.route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createPayment, getPaymentById } from '../controllers/payment.controller.js';
-
-const router = express.Router();
-
-router.post('/create', createPayment);
-router.get('/:id', getPaymentById);
-
-export default router;
